c8f3ed0dead51dbed93e7db20d49f855ab4c103b
Sergei Krivochenko <admin@krivochenko.ru>
2024-07-09 18:58:33 -0300

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useCallback } from 'react';
 import './App.css';
 
 function App() {
+  const click = useCallback(() => {
+    (window as any).Telegram.WebApp.switchInlineQuery('test', ['users', 'bots', 'groups', 'channels']);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        {JSON.stringify((window as any).Telegram.WebApp)}
+      </p>
+      <button onClick={click}>Click</button>
     </div>
   );
 }
